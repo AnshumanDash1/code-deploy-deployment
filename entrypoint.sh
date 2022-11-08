c40f53411114442dc030a8984bb9f5960dd17031
AnshumanDash1 <105815299+AnshumanDash1@users.noreply.github.com>
2022-11-08 10:48:53 -0500

    updated output method

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,8 @@ echo ${INPUT_S3_BUCKET_NAME}
 test_output_inEntryPoint=${INPUT_S3_BUCKET_NAME}
 echo $test_output_inEntryPoint
 
-echo "::set-output name=test_output_inEntryPoint::$test_output_inEntryPoint"
+# echo "::set-output name=test_output_inEntryPoint::$test_output_inEntryPoint"
 
-# echo "{name}=
+echo "{name}={test_output_inEntryPoint}" >> $GITHUB_OUTPUT
 
 echo "Should have a line above this"
