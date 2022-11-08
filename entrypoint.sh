99d7cac40295c79f08f0f79ad0d98de44573698e
AnshumanDash1 <105815299+AnshumanDash1@users.noreply.github.com>
2022-11-08 16:31:45 -0500

    Uncommented main code deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
 echo "the entrypoint.sh file started!"
-
-echo ${INPUT_S3_BUCKET_NAME} : echos the bucket name through input
-
-test_output_inEntryPoint=${INPUT_S3_BUCKET_NAME}
-echo $test_output_inEntryPoint : echos bucket name through bash var
-
-aws s3 ls
-
 ## separating the bucket name and key from full path name:
 
 
@@ -21,23 +13,23 @@ aws s3 ls
 # sudo yum -y update 
 # sudo yum -y install aws-cli
 
-# ## start of the code deploy section
-# s3_bucket_name=${INPUT_S3_BUCKET_NAME}
-# s3_location=${INPUT_S3_BUCKET_LOCATION}
-# deploy_desc=${INPUT_DESCRIPTION_FOR_DEPLOYMENT}
-# app_name=${INPUT_APPLICATION_NAME}
-# deploy_name=${INPUT_DEPLOYMENT_GROUP_NAME}
-# deploy_config_name=${INPUT_DEPLOYMENT_CONFIG_NAME}
-# bundle_type=${INPUT_BUNDLE_TYPE}
+## start of the code deploy section
+s3_bucket_name=${INPUT_S3_BUCKET_NAME}
+s3_location=${INPUT_S3_BUCKET_LOCATION}
+deploy_desc=${INPUT_DESCRIPTION_FOR_DEPLOYMENT}
+app_name=${INPUT_APPLICATION_NAME}
+deploy_name=${INPUT_DEPLOYMENT_GROUP_NAME}
+deploy_config_name=${INPUT_DEPLOYMENT_CONFIG_NAME}
+bundle_type=${INPUT_BUNDLE_TYPE}
 
 
-# ETAG=`aws s3api head-object --bucket $s3_bucket_name --key $s3_location --query ETag --output text`
-# aws deploy create-deployment \
-#   --description $deploy_desc \
-#   --application-name $app_name \
-#   --deployment-group-name $deploy_name \
-#   --deployment-config-name $deploy_config \
-#   --s3-location bucket=$s3_bucket_name,key=$s3_location,bundleType=$bundle_type,eTag=$ETAG
+ETAG=`aws s3api head-object --bucket $s3_bucket_name --key $s3_location --query ETag --output text`
+aws deploy create-deployment \
+  --description $deploy_desc \
+  --application-name $app_name \
+  --deployment-group-name $deploy_name \
+  --deployment-config-name $deploy_config \
+  --s3-location bucket=$s3_bucket_name,key=$s3_location,bundleType=$bundle_type,eTag=$ETAG
   
-#   echo "entrypoint.sh file finished."
+  echo "entrypoint.sh file finished."
 
