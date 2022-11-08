127ac07aba5d3e891091f442a39b037e0c61b06a
AnshumanDash1 <105815299+AnshumanDash1@users.noreply.github.com>
2022-11-08 13:07:34 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,14 @@
 
 echo "this is a test"
 
-echo ${INPUT_S3_BUCKET_NAME}
+echo ${INPUT_S3_BUCKET_NAME} : echos the bucket name through input
 
 test_output_inEntryPoint=${INPUT_S3_BUCKET_NAME}
-echo $test_output_inEntryPoint
+echo $test_output_inEntryPoint : echos bucket name through bash var
 
 # echo "::set-output name=test_output_inEntryPoint::$test_output_inEntryPoint"
 
 echo "{name}={test_output_inEntryPoint}" >> $GITHUB_OUTPUT
+echo $GITHUB_OUTPUT
 
 echo "Should have a line above this"
