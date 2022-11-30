fc222393ec710400f04afcaedb28445b4f492650
AnshumanDash1 <105815299+AnshumanDash1@users.noreply.github.com>
2022-11-30 12:35:00 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@ echo "the entrypoint.sh file started!"
 chmod +x extract-s3-from-arn.sh
 ./extract-s3-from-arn.sh
 
-echo $s3_bucket_name - echos the bucket name through input
+echo $s3_bucket_name : echos the bucket name through input
 
-echo $s3_location - echos bucket name through bash var
+echo $s3_location : echos bucket name through bash var
 
 aws s3 ls
 
