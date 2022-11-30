ede761933eff82943aac8dfcaf7f0db8da4573f6
AnshumanDash1 <105815299+AnshumanDash1@users.noreply.github.com>
2022-11-30 13:15:28 -0500

    removed redundancy

--- extract-s3-from-arn.sh
+++ extract-s3-from-arn.sh
@@ -5,9 +5,8 @@ arn=${INPUT_S3_BUCKET_ARN}
 prefix=s3://
 pathName=$(echo $arn | sed "s,$prefix,,")
 
-bucket_name=$( echo $pathName | sed 's,/.*,,' )
+s3_bucket_name=$( echo $pathName | sed 's,/.*,,' )
 
-key=$( echo $pathName | sed "s,$bucket_name\/,," )
+s3_location=$( echo $pathName | sed "s,$bucket_name\/,," )
 
-s3_bucket_name=$bucket_name
-s3_location=$key
+
