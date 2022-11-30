4a749673981a6d02a9506c155b7e5f0c717fe01d
AnshumanDash1 <105815299+AnshumanDash1@users.noreply.github.com>
2022-11-30 09:56:01 -0500

    changed var names

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,11 @@
 
 echo "the entrypoint.sh file started!"
 
-echo ${INPUT_S3_BUCKET_NAME} : echos the bucket name through input
+./extract-s3-from-arn.sh
 
-test_output_inEntryPoint=${INPUT_S3_BUCKET_NAME}
-echo $test_output_inEntryPoint : echos bucket name through bash var
+echo $s3_bucket_name : echos the bucket name through input
+
+echo $s3_location : echos bucket name through bash var
 
 aws s3 ls
 
