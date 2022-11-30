7501372c25a890d17f510a906d3992139a7d8148
AnshumanDash1 <105815299+AnshumanDash1@users.noreply.github.com>
2022-11-30 12:48:57 -0500

    added code deploy part

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,10 +10,22 @@ echo $s3_location : echos bucket name through bash var
 
 aws s3 ls
 
-# echo "::set-output name=test_output_inEntryPoint::$test_output_inEntryPoint"
+## start of the code deploy section
+deploy_desc=${INPUT_DESCRIPTION_FOR_DEPLOYMENT}
+app_name=${INPUT_APPLICATION_NAME}
+deploy_name=${INPUT_DEPLOYMENT_GROUP_NAME}
+deploy_config_name=${INPUT_DEPLOYMENT_CONFIG_NAME}
+bundle_type=${INPUT_BUNDLE_TYPE}
 
-# echo "testName=test_output_inEntryPoint" >> $GITHUB_OUTPUT
-# echo $GITHUB_OUTPUT
+
+ETAG=`aws s3api head-object --bucket $s3_bucket_name --key $s3_location --query ETag --output text`
+aws deploy create-deployment \
+  --description $deploy_desc \
+  --application-name $app_name \
+  --deployment-group-name $deploy_name \
+  --deployment-config-name $deploy_config \
+  --s3-location bucket=$s3_bucket_name,key=$s3_location,bundleType=$bundle_type,eTag=$ETAG
+
 
 
 
